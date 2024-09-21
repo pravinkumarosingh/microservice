cd565b11b2e3ddbe7dbd67e312209373b7cbd990
pravinkumarosingh <pravinkumarosingh@gmail.com>
2024-09-21 10:52:03 +0530

    InventoryClient added in order service to call API using FeignClient

--- order/order/src/main/java/com/service/order/service/OrderService.java
+++ order/order/src/main/java/com/service/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.service.order.service;
 
+import com.service.order.client.InventoryClient;
 import com.service.order.dto.OrderRequest;
 import com.service.order.model.Order;
 import com.service.order.repository.OrderRepository;
@@ -15,16 +16,26 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = Order.builder()
-                .orderNo(UUID.randomUUID().toString())
-                .skuCode(orderRequest.getSkuCode())
-                .price(orderRequest.getPrice())
-                .quantity(orderRequest.getQuantity())
-                .build();
-        orderRepository.save(order);
-        log.info("Place {} order" , order.getSkuCode());
+
+        boolean isProductInStock = inventoryClient.isInStock(orderRequest.getSkuCode(), orderRequest.getQuantity());
+
+        if (isProductInStock){
+            Order order = Order.builder()
+                    .orderNo(UUID.randomUUID().toString())
+                    .skuCode(orderRequest.getSkuCode())
+                    .price(orderRequest.getPrice())
+                    .quantity(orderRequest.getQuantity())
+                    .build();
+            orderRepository.save(order);
+            log.info("Place {} order" , order.getSkuCode());
+        } else {
+            throw new RuntimeException("Product with SKU Code " + orderRequest.getSkuCode() + " is not in stock");
+        }
+
+
     }
 
 }
